52be4d97528000d6a68431522d2b34dd03972954
Дария Бурлова <dariaburlova@gmail.com>
2022-02-13 20:42:55 +0300

    1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,9 @@
-fun main(){
-    val likeAm = 61
-    var text = ""
-
-    if(likeAm % 10 == 1 && likeAm % 100 != 11){
-        text = "человеку"
-    }else {
-        text ="людям"
+fun main() {
+    val likeAm = 11 // И с 10, и с 11, и с 1111 работает, как и должно
+    val result = if (likeAm % 10 == 1 && likeAm % 100 != 11) {
+        "человеку"
+    } else {
+        "людям"
     }
-    println("Понравилось $likeAm $text")
+    println("Понравилось $likeAm $result")
 }
